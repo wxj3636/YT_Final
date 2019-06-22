36fc034e879aa830eec4c12422713a05c36d8898
Cole <cwtrexler@ymail.com>
2019-06-22 11:44:49 -0600

    Attempted GUI, crashes, has dictionraies

--- main.py
+++ main.py
@@ -1,26 +1,100 @@
 from pytube import YouTube as yt
 import os
 import time
-
+import numpy as np
+import pickle
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTableWidget, QTableWidgetItem
+
+# Gets the current working directory before the program runs
+cwd = os.getcwd()
 
 
+# -------------------- Function definitions --------------------
+# Creates data folder, and subfolders
+def create_data_folder():
+    data_folder = cwd + "/data"
+    if not os.path.isdir(data_folder):
+        os.makedirs(data_folder)
+
+    img_folder = data_folder + "/img"
+    if not os.path.isdir(img_folder):
+        os.makedirs(img_folder)
+
+    audio_folder = data_folder + "/audio"
+    if not os.path.isdir(audio_folder):
+        os.makedirs(audio_folder)
+
+
+# Saves dictionary passed
+def save_dict(temp_dict):
+    with open(cwd + '/data/data.dat', 'wb') as fp:
+        pickle.dump(temp_dict, fp)
+
+
+# Loads dictionary passed
+def load_dict():
+    with open(cwd + '/data/data.dat', 'rb') as f:
+        out_dict = pickle.load(f)
+        return out_dict
+
+
+class TableView(QTableWidget):
+    def __init__(self, data, *args):
+        QTableWidget.__init__(self, *args)
+        self.data = data
+        self.setData()
+        self.resizeRowsToContents()
+        self.resizeColumnsToContents()
+
+    def setData(self):
+        horHeaders = []
+        for n, key in enumerate(sorted(self.data.keys())):
+            horHeaders.append(key)
+            for m, item in enumerate(self.data[key]):
+                newitem = QTableWidgetItem(item)
+                self.setItem(n, m, newitem)
+        self.setHorizontalHeaderLabels(horHeaders)
+
+
+# Displays the GUI
 def app():
     main_app = QApplication(sys.argv)
     window = QWidget()
     window.setWindowTitle("Testing this mug")
-    layout = QVBoxLayout()
-    layout.addWidget(QPushButton('Top'))
-    layout.addWidget(QPushButton('Bottom'))
-    window.setLayout(layout)
+    # layout = QVBoxLayout()
+    # layout.addWidget(QPushButton('Top'))
+    # layout.addWidget(QPushButton('Bottom'))
+    # window.setLayout(layout)
     window.show()
     main_app.exec_()
 
 
-app()
-print("TEST")
+# -------------------- Start of main program --------------------
+create_data_folder()
+# app()
+# my_dict = {
+#     'Name1': {'Name': 'Nightcore - Destiny', 'URL': 'https://www.youtube.com/watch?v=3caAitEusSw'},
+#     'Name2': {'Name': 'Nightcore - BLACKOUT', 'URL': 'https://www.youtube.com/watch?v=HZYihvoBBGw'}
+# }
+# my_dict = {
+#     'ID1': ['Nightcore - Destiny', 'https://www.youtube.com/watch?v=3caAitEusSw'],
+#     'ID2': ['Nightcore - BLACKOUT', 'https://www.youtube.com/watch?v=HZYihvoBBGw']
+# }
 
+
+
+save_dict("")
+try:
+    my_dict_2 = load_dict()
+
+except TypeError:
+    print("No file or data in file")
+
+app = QApplication(sys.argv)
+table = TableView(my_dict_2, 2, 2)
+table.show()
+sys.exit(app.exec_())
 
 # # File name for text file with YouTube links to download
 # infile_name = "YouTube Download Audio.txt"
